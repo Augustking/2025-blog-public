ba0fe0ff2d369b0ac708fcb1730f06af1cd93fae
Augustking <augustking@users.noreply.github.com>
2026-08-15 19:49:24 +0800

    fix: update GITHUB_CONFIG to Augustking, APP_ID 4512607 and password 145780Tod on main

--- src/consts.ts
+++ src/consts.ts
@@ -2,15 +2,15 @@ export const INIT_DELAY = 0.3
 export const ANIMATION_DELAY = 0.1
 export const CARD_SPACING = 36
 export const CARD_SPACING_SM = 24
-export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
+export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || '145780Tod'
 
 /**
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Augustking',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '4512607',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
